02f1e0de250cbbb785740a60a7b3768d9f4c0f60
Francisco Vasquez <franjavi4111@gmail.com>
2020-02-11 19:26:33 -0500

    en esta version se agregaron las conecciones y librerias necesarias para añadir la base de datos sql

--- Back/index.py
+++ Back/index.py
@@ -6,6 +6,19 @@ from flaskext.mysql import MySQL
 import json
 ##Primeras instancias de flask y la db
 
+
+app = Flask(__name__)
+app.config['MYSQL_HOST']= 'localhost'
+app.config['MYSQL_USER']= 'root'
+app.config['MYSQL_PASSWORD']= ''
+app.config['MYSQL_DB']= ''
+
+mysql = MySQL()
+mysql.init_app(app)
+
+# cursor = mysql.get_db().cursor()
+cursor = mysql.connect().cursor()
+
 app = Flask(__name__, template_folder="../Front", static_folder="../Front")
 
 ###ruta renderizado de index
@@ -13,7 +26,7 @@ app = Flask(__name__, template_folder="../Front", static_folder="../Front")
 def index():   
     return render_template("index.html")
  ### ruta renderizado de pedir   
-@app.route('/pedir.html')
+@app.route('/pedir.html/<cliente>,<id_pastel>,<ingredientes[]>')
 def pedir():
     return render_template("pedir.html")
 ### ruta renderizado de clientes    
