d1891c523a4f82d945dd3e45396afab48d56e6ea
Francisco Vasquez <franjavi4111@gmail.com>
2020-02-16 16:31:52 -0500

    cambios en index, se corrigen errores de rutas y se capturan variables del form crear ademas se crea form en pedir

--- Back/index.py
+++ Back/index.py
@@ -6,27 +6,26 @@ from flaskext.mysql import MySQL
 import json
 ##Primeras instancias de flask y la db
 
-
-app = Flask(__name__)
-app.config['MYSQL_HOST']= 'localhost'
-app.config['MYSQL_USER']= 'root'
-app.config['MYSQL_PASSWORD']= ''
-app.config['MYSQL_DB']= ''
-
-mysql = MySQL()
-mysql.init_app(app)
-
-# cursor = mysql.get_db().cursor()
-cursor = mysql.connect().cursor()
-
 app = Flask(__name__, template_folder="../Front", static_folder="../Front")
 
 ###ruta renderizado de index
-@app.route('/index.html')
-def index():   
-    return render_template("index.html")
+@app.route('/', methods=['GET'] )
+def index(): 
+    try:
+        nombre = request.args.get('nombre')
+        direccion = request.args.get('direccion')
+        telefono = request.args.get('telefono')
+        id = request.args.get('id/nit')
+        tipo = request.args.get('tipo')
+        if (nombre != ''):
+            return 'Hola ' + nombre
+        else:
+            return render_template('index.html')
+    except:
+        return render_template('index.html')
+        
  ### ruta renderizado de pedir   
-@app.route('/pedir.html/<cliente>,<id_pastel>,<ingredientes[]>')
+@app.route('/pedir.html')
 def pedir():
     return render_template("pedir.html")
 ### ruta renderizado de clientes    
