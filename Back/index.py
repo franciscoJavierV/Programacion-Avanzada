3004b489b3c21e4bb807ad9ee53a971a26bf2c07
RedMoonPie <die.fer04@hotmail.com>
2020-02-22 14:02:03 -0500

    m
    m

--- Back/index.py
+++ Back/index.py
@@ -2,14 +2,25 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
+
+
+
 import json
 ##Primeras instancias de flask y la db
 
 app = Flask(__name__, template_folder="../Front", static_folder="../Front")
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Dieguito123'
+app.config['MYSQL_DB'] = 'flaskcrud'
+mysql = MySQL(app)
+
+
 ###ruta renderizado de index
-@app.route('/', methods=['GET'] )
+@app.route('/', methods=['GET'])
+@app.route('/index.html')
 def index(): 
     try:
         nombre = request.args.get('nombre')
@@ -17,12 +28,18 @@ def index():
         telefono = request.args.get('telefono')
         id = request.args.get('id/nit')
         tipo = request.args.get('tipo')
+
+
         if (nombre != ''):
             return 'Hola ' + nombre
         else:
             return render_template('index.html')
     except:
         return render_template('index.html')
+
+
+
+
         
  ### ruta renderizado de pedir   
 @app.route('/pedir.html')
